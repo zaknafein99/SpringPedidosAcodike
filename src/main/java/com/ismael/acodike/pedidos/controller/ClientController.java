224ed167086282a3b834937825c709645965b138
Ismael Marquez <imarquez99@gmail.com>
2022-06-27 11:36:29 -0300

    Add Swagger dependecies and configuration for controllers

--- src/main/java/com/ismael/acodike/pedidos/controller/ClientController.java
+++ src/main/java/com/ismael/acodike/pedidos/controller/ClientController.java
@@ -2,6 +2,10 @@ package com.ismael.acodike.pedidos.controller;
 
 import com.ismael.acodike.pedidos.dto.ClientDTO;
 import com.ismael.acodike.pedidos.service.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Client", description = "Operations about clients")
 @RequestMapping("/api/v1")
 @RestController
 public class ClientController {
@@ -22,24 +27,48 @@ public class ClientController {
         this.service = service;
     }
 
+    @Operation(summary = "Get all clients", description = "Get all clients")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of clients"),
+            @ApiResponse(responseCode = "403", description = "Access to the resource requested is forbidden"),
+            @ApiResponse(responseCode = "404", description = "The resource requested does not exist")
+    })
     @GetMapping("/clientes")
     public ResponseEntity<List<ClientDTO>> getAllClients() {
         List<ClientDTO> client = service.getAll();
         return ResponseEntity.ok(client);
     }
 
+    @Operation(summary = "Get client by id", description = "Get client by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved client"),
+            @ApiResponse(responseCode = "403", description = "Access to the resource requested is forbidden"),
+            @ApiResponse(responseCode = "404", description = "The resource requested does not exist")
+    })
     @GetMapping("/clientes/{id}")
     public ResponseEntity<ClientDTO> getClienteById(@PathVariable Integer id) {
         ClientDTO client = service.getById(id);
         return ResponseEntity.ok(client);
     }
 
+    @Operation(summary = "Get client by telephone", description = "Get client by telephone")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved client by telephone"),
+            @ApiResponse(responseCode = "403", description = "Access to the resource requested is forbidden"),
+            @ApiResponse(responseCode = "404", description = "The resource requested does not exist")
+    })
     @GetMapping("/clientes/telefono/{telefono}")
     public ResponseEntity<List<ClientDTO>> getClienteByTelephone(@PathVariable String telefono) {
         List<ClientDTO> client = service.getByTelephone(telefono);
         return ResponseEntity.ok(client);
     }
 
+    @Operation(summary = "Create client", description = "Create client")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully created client"),
+            @ApiResponse(responseCode = "403", description = "Access to the resource requested is forbidden"),
+            @ApiResponse(responseCode = "404", description = "The resource requested does not exist")
+    })
     @PostMapping(path = "/clientes", consumes = "application/json", produces = "application/json")
     public ResponseEntity<ClientDTO> createClient(@RequestBody ClientDTO client) {
         ClientDTO newClient = service.create(client);
--- src/main/java/com/ismael/acodike/pedidos/controller/ItemController.java
+++ src/main/java/com/ismael/acodike/pedidos/controller/ItemController.java
@@ -2,6 +2,10 @@ package com.ismael.acodike.pedidos.controller;
 
 import com.ismael.acodike.pedidos.dto.ItemDTO;
 import com.ismael.acodike.pedidos.service.ItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Item", description = "Operations about items")
 @RequestMapping("/api/v1")
 @RestController
 public class ItemController {
@@ -22,18 +27,36 @@ public class ItemController {
         this.service = service;
     }
 
+    @Operation(summary = "Get all items", description = "Get all items")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of items"),
+            @ApiResponse(responseCode = "403", description = "Access to the resource requested is forbidden"),
+            @ApiResponse(responseCode = "404", description = "The resource requested does not exist")
+    })
     @GetMapping("/items")
     public ResponseEntity<List<ItemDTO>> getAllItems() {
         List<ItemDTO> items = service.getAll();
         return ResponseEntity.ok(items);
     }
 
+    @Operation(summary = "Get item by id", description = "Get item by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved items"),
+            @ApiResponse(responseCode = "403", description = "Access to the resource requested is forbidden"),
+            @ApiResponse(responseCode = "404", description = "The resource requested does not exist")
+    })
     @GetMapping("/items/{id}")
     public ResponseEntity<ItemDTO> getItemById(@PathVariable Integer id) {
         ItemDTO item = service.getById(id);
         return ResponseEntity.ok(item);
     }
 
+    @Operation(summary = "Create item", description = "Create item")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully created item"),
+            @ApiResponse(responseCode = "403", description = "Access to the resource requested is forbidden"),
+            @ApiResponse(responseCode = "404", description = "The resource requested does not exist")
+    })
     @PostMapping(path = "/items", consumes = "application/json", produces = "application/json")
     public ResponseEntity<ItemDTO> createItem(@RequestBody ItemDTO item) {
         ItemDTO newItem = service.create(item);
